72da8ecb4cb6bb8676d7465096612578202100a3
Justin Shroyer <hjshroyer@hotmail.com>
2020-03-19 21:11:38 -0500

    More touch up work

--- js/shop.js
+++ js/shop.js
@@ -130,7 +130,7 @@ Basket.prototype.addProduct = function(e) {
     basket.items.push(newProduct);
     localStorage.setItem('savedCart', JSON.stringify(currentItems));
     setCartIcon();
-
+    alert("Item added to cart");
 }
 
 Basket.prototype.removeItem=function(id){
@@ -207,50 +207,3 @@ function getLocalCart() {
     var newItem = JSON.parse(localStorage.getItem('savedCart')) || [];
     return newItem;
   }
-//   // chart code thanks to https://www.chartjs.org/docs/latest/
-//   var ctx = document.getElementById('chart').getContext('2d');
-//   // eslint-disable-next-line no-undef
-//   new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//       // what does labels do?
-//       labels: get('sport'),
-//       // what does datasets do?
-//       // it's an array of objects
-//       datasets: [{
-//         // what does this label do?
-//         // key, legend
-//         label: '# of Votes',
-//         // what does this data do?
-//         // actually the values in the chart
-//         data: getPizzaArray('timesClicked'),
-//         backgroundColor: [
-//           'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//         ],
-//         borderColor: [
-//           'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//         ],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         yAxes: [{
-//           ticks: {
-//             beginAtZero: true,
-//             stepSize: 1
-//           }
-//         }]
-//       }
-//     }
-//   });
